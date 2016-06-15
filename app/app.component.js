1f53a6bb947414eb7fbd8157a2c82e7e9f29b8bb
Hossein Aarabi <hossein.aarabi.work@gmail.com>
2016-06-14 23:44:59 -0700

    building on the angular sample app - master/detail ui pattern

--- app/app.component.js
+++ app/app.component.js
@@ -16,23 +16,24 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 // Best practice: to create component per purpose/functionality
 // Here we are importing the Component decorator function.
 var core_1 = require('@angular/core');
-var Hero = (function () {
-    function Hero() {
+var Provider = (function () {
+    function Provider() {
     }
-    return Hero;
+    return Provider;
 }());
-exports.Hero = Hero;
+exports.Provider = Provider;
 // "Decorator" telling Angular what template to use and how to create the component.
 // The @Component decorator function takes in a metadata object, that tells angular how to create
 //  and use this component.
 var AppComponent = (function () {
     function AppComponent() {
-        this.title = 'Providers';
-        this.hero = {
-            id: 1,
-            name: 'Dr. Frank'
-        };
+        this.title = 'Provider Search';
+        // We don't need to define the providers type, TS can infer it from the PROVIDERS aaray.
+        this.providers = PROVIDERS;
     }
+    AppComponent.prototype.onSelect = function (provider) {
+        this.selectedProvider = provider;
+    };
     AppComponent = __decorate([
         core_1.Component({
             // This specifies a simple CSS selector for an HTML element to represent the component
@@ -40,12 +41,30 @@ var AppComponent = (function () {
             // This tells Angular how to render this component. 
             // double curly braces {{}} is called "interpolation" - this is one-way binding
             // ngModel is a build directive that allows two-way binding. It also propagates changes to
-            //  every other binding of the hero.name.
-            template: "\n        <h1>{{title}}</h1>\n        <h2>{{hero.name}} details!</h2>\n        <div>\n            <label>id: </label>{{hero.id}}\n        </div>\n        <div>\n            <label>name: </label>\n            <input [(ngModel)]=\"hero.name\" placeholder=\"name\">\n        </div>\n    "
+            //  every other binding of the provider.name.
+            // The (*) prefix to ngFor indicates that the <li> element and its children constitute a master
+            //  template
+            // ngFor and ngIf are "structural directives" because they can change the structure of DOM.
+            template: "\n        <h1>{{title}}</h1>\n        <h2>Provider List</h2>\n        <ul class=\"providers\">\n            <li *ngFor=\"let provider of providers\" \n                [class.selected]=\"provider === selectedProvider\"\n                (click)=\"onSelect(provider)\">\n                <span class=\"badge\">{{provider.id}}</span> {{provider.name}}\n            </li>\n        </ul>\n        <div *ngIf=\"selectedProvider\">\n            <h2>{{selectedProvider.name}} details!</h2>\n            <div><label>id: </label>{{selectedProvider.id}}</div>\n            <div>\n                <label>name: </label>\n                <input [(ngModel)]=\"selectedProvider.name\" placeholder=\"name\"/>\n            </div>\n        </div>\n    ",
+            // the style applied in the decorator is only specific to this component. The outer HTML is not
+            //  affected.
+            styles: ["\n    .selected {\n        background-color: #CFD8DC !important;\n        color: white;\n    }\n    .providers {\n        margin: 0 0 2em 0;\n        list-style-type: none;\n        padding: 0;\n        width: 15em;\n    }\n    .providers li {\n        cursor: pointer;\n        position: relative;\n        left: 0;\n        background-color: #EEE;\n        margin: .5em;\n        padding: .3em 0;\n        height: 1.6em;\n        border-radius: 4px;\n    }\n    .providers li.selected:hover {\n        background-color: #BBD8DC !important;\n        color: white;\n    }\n    .providers li:hover {\n        color: #607D8B;\n        background-color: #DDD;\n        left: .1em;\n    }\n    .providers .text {\n        position: relative;\n        top: -3px;\n    }\n    .providers .badge {\n        display: inline-block;\n        font-size: small;\n        color: white;\n        padding: 0.8em 0.7em 0 0.7em;\n        background-color: #607D8B;\n        line-height: 1em;\n        position: relative;\n        left: -1px;\n        top: -4px;\n        height: 1.8em;\n        margin-right: .8em;\n        border-radius: 4px 0 0 4px;\n    }\n    "]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
 }());
 exports.AppComponent = AppComponent;
+var PROVIDERS = [
+    { "id": 11, "name": "Dr. John" },
+    { "id": 12, "name": "Dr. Smith" },
+    { "id": 13, "name": "Dr. Dan" },
+    { "id": 14, "name": "Dr. Robert" },
+    { "id": 15, "name": "Dr. Jack" },
+    { "id": 16, "name": "Dr. Alan" },
+    { "id": 17, "name": "Dr. Mike" },
+    { "id": 18, "name": "Dr. Joe" },
+    { "id": 19, "name": "Dr. Philip" },
+    { "id": 20, "name": "Dr. Sam" }
+];
 //# sourceMappingURL=app.component.js.map
--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,7 @@
 // Here we are importing the Component decorator function.
 import { Component } from '@angular/core';
 
-export class Hero {
+export class Provider {
   id: number;
   name: string;
 }
@@ -21,27 +21,108 @@ export class Hero {
     // This tells Angular how to render this component. 
     // double curly braces {{}} is called "interpolation" - this is one-way binding
     // ngModel is a build directive that allows two-way binding. It also propagates changes to
-    //  every other binding of the hero.name.
+    //  every other binding of the provider.name.
+    // The (*) prefix to ngFor indicates that the <li> element and its children constitute a master
+    //  template
+    // ngFor and ngIf are "structural directives" because they can change the structure of DOM.
     template: `
         <h1>{{title}}</h1>
-        <h2>{{hero.name}} details!</h2>
-        <div>
-            <label>id: </label>{{hero.id}}
+        <h2>Provider List</h2>
+        <ul class="providers">
+            <li *ngFor="let provider of providers" 
+                [class.selected]="provider === selectedProvider"
+                (click)="onSelect(provider)">
+                <span class="badge">{{provider.id}}</span> {{provider.name}}
+            </li>
+        </ul>
+        <div *ngIf="selectedProvider">
+            <h2>{{selectedProvider.name}} details!</h2>
+            <div><label>id: </label>{{selectedProvider.id}}</div>
+            <div>
+                <label>name: </label>
+                <input [(ngModel)]="selectedProvider.name" placeholder="name"/>
+            </div>
         </div>
-        <div>
-            <label>name: </label>
-            <input [(ngModel)]="hero.name" placeholder="name">
-        </div>
-    `
+    `,
+    // the style applied in the decorator is only specific to this component. The outer HTML is not
+    //  affected.
+    styles:[`
+    .selected {
+        background-color: #CFD8DC !important;
+        color: white;
+    }
+    .providers {
+        margin: 0 0 2em 0;
+        list-style-type: none;
+        padding: 0;
+        width: 15em;
+    }
+    .providers li {
+        cursor: pointer;
+        position: relative;
+        left: 0;
+        background-color: #EEE;
+        margin: .5em;
+        padding: .3em 0;
+        height: 1.6em;
+        border-radius: 4px;
+    }
+    .providers li.selected:hover {
+        background-color: #BBD8DC !important;
+        color: white;
+    }
+    .providers li:hover {
+        color: #607D8B;
+        background-color: #DDD;
+        left: .1em;
+    }
+    .providers .text {
+        position: relative;
+        top: -3px;
+    }
+    .providers .badge {
+        display: inline-block;
+        font-size: small;
+        color: white;
+        padding: 0.8em 0.7em 0 0.7em;
+        background-color: #607D8B;
+        line-height: 1em;
+        position: relative;
+        left: -1px;
+        top: -4px;
+        height: 1.8em;
+        margin-right: .8em;
+        border-radius: 4px 0 0 4px;
+    }
+    `]
 })
 
 // "Component class" controlling the appearance and behavior of view via its template. This is
 //  where the application logic is written.
 // This component is exported so that we can import it elsewhere in the application.
 export class AppComponent {
-    title = 'Providers';
-    hero: Hero = {
-        id: 1,
-        name: 'Dr. Frank'
-    };
+    title = 'Provider Search';
+    
+    // We don't need to define the providers type, TS can infer it from the PROVIDERS aaray.
+    providers = PROVIDERS;    
+    selectedProvider: Provider;
+    
+    onSelect(provider: Provider) { 
+        this.selectedProvider = provider; 
+    }
  }
+
+ var PROVIDERS: Provider[] = [
+     { "id": 11, "name": "Dr. John" },
+     { "id": 12, "name": "Dr. Smith" },
+     { "id": 13, "name": "Dr. Dan" },
+     { "id": 14, "name": "Dr. Robert" },
+     { "id": 15, "name": "Dr. Jack" },
+     { "id": 16, "name": "Dr. Alan" },
+     { "id": 17, "name": "Dr. Mike" },
+     { "id": 18, "name": "Dr. Joe" },
+     { "id": 19, "name": "Dr. Philip" },
+     { "id": 20, "name": "Dr. Sam" }
+ ];
+
+
